7a8b1839937278ce4d06094e5a6192d561c8efba
sofia-pinto <sofia.pinto@ort.org.br>
2026-02-25 15:36:26 -0300

    Finalizando o número 1

---
+++ Numero2.py
@@ -0,0 +1,8 @@
+#Número 2
+
+base = float(input("Informe a base do triângulo: "))
+altura = float(input("Informe a altura do triângulo: "))
+area = base * altura
+perimetro = (base * 2) + (altura * 2)
+print("Área: ", area)
+print("Perímetro: ", perimetro)
