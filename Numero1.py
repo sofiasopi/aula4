896f4af4acc059a2fcae0da9f3b8891b92f32b6d
sofia-pinto <sofia.pinto@ort.org.br>
2026-02-25 15:23:04 -0300

    Finalizando o número 1

---
+++ Numero1.py
@@ -0,0 +1,8 @@
+#Número 2
+
+N1 = float(input("Informe a nota 1:"))
+N2 = float(input("Informe a nota 2:"))
+N3 = float(input("Informe a nota 3:"))
+N4 = float(input("Informe a nota 4:"))
+media = (N1 + N2 + N3 + N4) / 4
+print("Média: ", media)
